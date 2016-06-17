1c70a6acef11020d3563c372c59b93e45a87f546
Nathanael Thompson <nathanael.thompson90@gmail.com>
2016-06-17 11:33:27 -0400

    Initial Commit

---
+++ RVSampleApp/CustomControlWindow.xaml.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace RVSampleApp
+{
+    /// <summary>
+    /// Interaction logic for CustomControlWindow.xaml
+    /// </summary>
+    public partial class CustomControlWindow : Window
+    {
+        public CustomControlWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void closeButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void setGradientButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                int red1 = int.Parse(rbox1.Text);
+                int green1 = int.Parse(gbox1.Text);
+                int blue1 = int.Parse(bbox1.Text);
+                int red2 = int.Parse(rbox2.Text);
+                int green2 = int.Parse(gbox2.Text);
+                int blue2 = int.Parse(bbox2.Text);
+                LinearGradientBrush lgb = new LinearGradientBrush(
+                    Color.FromRgb((byte)red1, (byte)green1, (byte)blue1),
+                    Color.FromRgb((byte)red2, (byte)green2, (byte)blue2),
+                    new Point(0.5, 0),
+                    new Point(0.5, 1));
+                customControlTest.ccBorder.Background = lgb;
+            }
+            catch(ArgumentNullException)
+            {
+                MessageBox.Show("RGB values cannot be empty!", "RGB Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            catch(FormatException)
+            {
+                MessageBox.Show("RGB values can only be integers from 0 to 255!", "RGB Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void setBorderButton_Click(object sender, RoutedEventArgs e)
+        {
+            double borderThickness = 0.0;
+            try
+            {
+                borderThickness = double.Parse(borderThicknessTB.Text);
+                if(borderThickness < 0)
+                {
+                    MessageBox.Show("Border thickness cannot be less than 0.", "Border Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+                if(allBorderRB.IsChecked == true) //the IsChecked property *has* to check against a boolean literal
+                {
+                    customControlTest.ccBorder.BorderThickness = new Thickness(borderThickness);
+                }
+                else if(leftBorderRB.IsChecked == true)
+                {
+                    customControlTest.ccBorder.BorderThickness = new Thickness(borderThickness, 0, 0, 0);
+                }
+                else if(rightBorderRB.IsChecked == true)
+                {
+                    customControlTest.ccBorder.BorderThickness = new Thickness(0, 0, borderThickness, 0);
+                }
+                else if(topBorderRB.IsChecked == true)
+                {
+                    customControlTest.ccBorder.BorderThickness = new Thickness(0, borderThickness, 0, 0);
+                }
+                else if(bottomBorderRB.IsChecked == true)
+                {
+                    customControlTest.ccBorder.BorderThickness = new Thickness(0, 0, 0, borderThickness);
+                }
+            }
+            catch(FormatException)
+            {
+                MessageBox.Show("Value of thickness must be in a numeric form.", "Thickness Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
---
+++ RVSampleApp/MainWindow.xaml.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RVSampleApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            textOutBox.Document.Blocks.Clear();
+            rtbIn.Document.Blocks.Clear();
+        }
+
+        private void customCntrlButton_Click(object sender, RoutedEventArgs e)
+        {
+            var secondWindow = new CustomControlWindow();
+            secondWindow.Show();
+        }
+
+        private void exitButton_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void generateButton_Click(object sender, RoutedEventArgs e)
+        {
+            textOutBox.Document.Blocks.Clear();
+            string textIn = new TextRange(rtbIn.Document.ContentStart, rtbIn.Document.ContentEnd).Text;
+            StringBuilder prevText = new StringBuilder();
+            StringBuilder highlightText = new StringBuilder();
+            bool noHighlightFlag = true;
+
+            for (int i = 0; i < textIn.Length; i++)
+            {
+                if(textIn[i] != '#' && textIn[i] != '@' && textIn[i] != '\\') //if the current char isn't a delimiter
+                {
+                    prevText.Append(textIn[i]); //and it to the previously scanned chars
+                }
+                else if(textIn[i] == '#' || textIn[i] == '@') //if we have found one of the special chars
+                {
+                    //range for previously found output
+                    TextRange blRange = new TextRange(textOutBox.Document.ContentEnd, textOutBox.Document.ContentEnd);
+                    blRange.Text = prevText.ToString();
+                    blRange.ApplyPropertyValue(TextElement.ForegroundProperty, Brushes.Black);
+
+                    int j = i;
+                    do //we need the loop to execute at least once, the char at position i will break out of the while loop immediately otherwise
+                    {
+                        highlightText.Append(textIn[j]); //append until a special char is found
+                        j++;
+                    } //an ugly condition, but it works
+                    while (j < textIn.Length && textIn[j] != ' ' && textIn[j] != '\r' && textIn[j] != '#' && textIn[j] != '@');
+                    
+                    if (highlightText.ToString().Length > 1) //if a special char was not found immediately, change the color
+                    {
+                        TextRange trToAppend = new TextRange(textOutBox.Document.ContentEnd, textOutBox.Document.ContentEnd);
+                        trToAppend.Text = highlightText.ToString() + " ";
+                        trToAppend.ApplyPropertyValue(TextElement.ForegroundProperty, Brushes.Blue);
+                    }
+                    else //otherwise, don't change color
+                    {
+                        TextRange trToAppend = new TextRange(textOutBox.Document.ContentEnd, textOutBox.Document.ContentEnd);
+                        highlightText.Append(textIn[j]);
+                        trToAppend.Text = highlightText.ToString();
+                        trToAppend.ApplyPropertyValue(TextElement.ForegroundProperty, Brushes.Black); 
+                    }
+                    prevText.Clear();
+                    highlightText.Clear();
+                    noHighlightFlag = false;
+                    i = j;
+                }
+            }
+            if(noHighlightFlag || prevText.ToString() != "")//if there has been no change to the text, simply add it to the output
+            {
+                TextRange trToAppend = new TextRange(textOutBox.Document.ContentEnd, textOutBox.Document.ContentEnd);
+                trToAppend.Text = prevText.ToString();
+                trToAppend.ApplyPropertyValue(TextElement.ForegroundProperty, Brushes.Black);
+                
+            }
+        }
+    }
+}
